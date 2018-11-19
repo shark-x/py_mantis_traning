3237329af26a8180993e56ff45a00d088cb46566
karpeeva_anastasia <charming020793@gmail.com>
2018-11-19 15:19:14 +0700

    удален код для отладки

--- tests/test_add_project.py
+++ tests/test_add_project.py
@@ -17,5 +17,3 @@ def test_add_project(app):
     new_project_list = app.project.get_project_list()
     old_project_list.append(project)
     assert sorted(old_project_list, key=Project.id_or_max) == sorted(new_project_list, key=Project.id_or_max)
-    print(sorted(old_project_list, key=Project.id_or_max))
-    print(sorted(new_project_list, key=Project.id_or_max))
