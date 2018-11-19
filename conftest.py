759d36cd2403e88560cdcade8d170382cabbb512
karpeeva_anastasia <charming020793@gmail.com>
2018-11-19 17:20:55 +0700

    FTP - изменения файла конфигурации, для отключения captcha

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import pytest
 import json
 import os.path
 from fixture.application import Application
+import ftputil
 
 fixture = None
 target = None
@@ -19,14 +20,47 @@ def load_config(file):
 
 
 @pytest.fixture
-def app(request):
+def app(request, config):
     global fixture
     browser = request.config.getoption("--browser")
-    web_config = load_config(request.config.getoption("--target"))["webadmin"]
+    # web_config = load_config(request.config.getoption("--target"))["webadmin"]
+    web_config = config['webadmin']
     if fixture is None or not fixture.is_valid():
         fixture = Application(browser=browser, base_url=web_config["baseURL"])
-    fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
+    # fixture.session.ensure_login(username=web_config["username"], password=web_config["password"])
     return fixture
+
+
+@pytest.fixture(scope="session", autouse=True)
+def config(request):
+    return load_config(request.config.getoption("--target"))
+
+
+@pytest.fixture(scope="session", autouse=True)
+def configure_server(request, config):
+    install_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+
+    def fin():
+        restore_server_configuration(config['ftp']['host'], config['ftp']['username'], config['ftp']['password'])
+        request.addfinalizer(fin)
+
+
+def install_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            remote.remove("config_inc.php.bak")
+        if remote.path.isfile("config_inc.php"):
+            remote.rename("config_inc.php", "config_inc.php.bak")
+        remote.upload(os.path.join(os.path.dirname(__file__), "resources/config_inc.php"), "config_inc.php")
+
+
+def restore_server_configuration(host, username, password):
+    with ftputil.FTPHost(host, username, password) as remote:
+        if remote.path.isfile("config_inc.php.bak"):
+            if remote.path.isfile("config_inc.php"):
+                remote.remove("config_inc.php")
+            remote.rename("config_inc.php.bak", "config_inc.php")
+
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
@@ -35,6 +69,7 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
 
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="chrome")
